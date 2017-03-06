9b77e9094ac55ceb3e2e70bbbbacc28585823373
Jazzy <fairy7823@gmail.com>
2017-03-06 08:51:51 -0800

    check 2 part 1
    text cells and some processcommand

---
+++ TextExcel/src/textExcel/SpreadsheetLocation.java
@@ -0,0 +1,28 @@
+package textExcel;
+
+//Update this file with your own code.
+
+public class SpreadsheetLocation implements Location
+{
+	private int row;
+	private int column;
+	
+    @Override
+    public int getRow()
+    {
+        return row;
+    }
+
+    @Override
+    public int getCol()
+    {
+        return column;
+    }
+    
+    public SpreadsheetLocation(String cellName)
+    {
+        column = cellName.charAt(0)-65;
+        row = Integer.parseInt(cellName.substring(1))-1;
+    }
+
+}
